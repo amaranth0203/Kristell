5b619ec9910c783910211330cb62e30eb738ce3d
qiyunhu <qiyunhu@bbktel.com>
2015-09-14 20:10:21 +0800

    can add entity user and show user's id

--- app/src/main/java/com/mycompany/kristell/MainActivity.java
+++ app/src/main/java/com/mycompany/kristell/MainActivity.java
@@ -1,6 +1,7 @@
 package com.mycompany.kristell;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.hardware.display.DisplayManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,19 +11,58 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.mycompany.DaoMaster;
+import com.mycompany.DaoSession;
 import com.mycompany.User;
+import com.mycompany.UserDao;
 
 import java.util.Date;
+import java.util.List;
+
+import de.greenrobot.dao.internal.DaoConfig;
 
 public class MainActivity extends AppCompatActivity {
 
+    private SQLiteDatabase db ;
+    private DaoMaster daoMaster ;
+    private DaoSession daoSession ;
+    private UserDao userDao ;
+    private User user ;
+
     public void testButtonClicked( View view ) {
-        User user = new User() ;
-        System.out.println( "[+] aaaa " + new Date().toString()) ;
+        System.out.println("[+] This is testButtonClicked function" + new Date().toString()) ;
+
+
         EditText editText = ( EditText )findViewById( R.id.editText_test ) ;
         String Message = editText.getText().toString() ;
         TextView textView = ( TextView )findViewById( R.id.textView_test ) ;
-        textView.setText( Message ) ;
+        textView.setText(Message) ;
+
+        DaoMaster.DevOpenHelper helper = new DaoMaster.DevOpenHelper(this, "test-db", null);
+        db = helper.getWritableDatabase();
+        daoMaster= new DaoMaster( db ) ;
+        daoSession = daoMaster.newSession() ;
+        userDao = daoSession.getUserDao() ;
+        user = new User( ) ;
+        user.setName("ben") ;
+        user.setPassword("ben's passwd") ;
+        user.setUserId(Long.parseLong(Message)) ;
+        userDao.insert(user) ;
+    }
+
+    public void showButtonClicked( View view ) {
+        System.out.println("[+] This is showButtonClicked function" + new Date().toString()) ;
+        DaoMaster.DevOpenHelper helper = new DaoMaster.DevOpenHelper(this, "test-db", null);
+        db = helper.getWritableDatabase();
+        daoMaster= new DaoMaster( db ) ;
+        daoSession = daoMaster.newSession() ;
+        userDao = daoSession.getUserDao() ;
+        List<User> users = userDao.loadAll() ;
+        TextView textView = ( TextView )findViewById( R.id.textView_test ) ;
+        String Message = "" ;
+        for( int i = 0 ; i < users.size() ; i ++ )
+            Message += users.get(i).getUserId() + "\n";
+        textView.setText(Message) ;
     }
 
     @Override
