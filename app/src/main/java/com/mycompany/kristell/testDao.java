35e2050267c9c5b7dd7b8d2a0a24030e89f5b07b
qiyunhu <qiyunhu@bbktel.com>
2015-09-14 09:26:43 +0800

    sec commit

--- app/src/main/java/com/mycompany/kristell/testDao.java
+++ app/src/main/java/com/mycompany/kristell/testDao.java
@@ -5,7 +5,7 @@ package com.mycompany.kristell;
  * Created by Administrator on 2015/9/10.
  */
 public class testDao {
-    private static final String GENERATED_SRC_DIR = "./app/gen-src";
+    private static  final String GENERATED_SRC_DIR = "./app/gen-src";
     public static void main(String[] args) throws Exception {
 //        Schema schema = new Schema(1, DaoGenerator.class.getPackage().getName());
     }
