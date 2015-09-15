b5ece0d3b099e65348429c0af8aadb205d76189a
qiyunhu <qiyunhu@bbktel.com>
2015-09-15 13:08:27 +0800

    implement add card to Kristell

--- app/src/main/java/com/mycompany/kristell/MainActivity.java
+++ app/src/main/java/com/mycompany/kristell/MainActivity.java
@@ -8,9 +8,18 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.mycompany.kristell.DAO.Card;
+import com.mycompany.kristell.DAO.CardDao;
+import com.mycompany.kristell.DAO.DaoMaster;
+import com.mycompany.kristell.DAO.DaoSession;
+import com.mycompany.kristell.DAO.Transaction;
+import com.mycompany.kristell.DAO.TransactionDao;
+
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
@@ -18,20 +27,30 @@ import de.greenrobot.dao.internal.DaoConfig;
 
 public class MainActivity extends AppCompatActivity {
 
-//    private SQLiteDatabase db ;
-//    private DaoMaster daoMaster ;
-//    private DaoSession daoSession ;
+    private SQLiteDatabase db ;
+    private DaoMaster daoMaster ;
+    private DaoSession daoSession ;
+    private TransactionDao transactionDao ;
+    private Transaction transaction ;
+    private CardDao cardDao ;
+    private Card card ;
 //    private UserDao userDao ;
 //    private User user ;
 
     public void testButtonClicked( View view ) {
         System.out.println("[+] This is testButtonClicked function" + new Date().toString()) ;
-
-
         EditText editText = ( EditText )findViewById( R.id.editText_test ) ;
         String Message = editText.getText().toString() ;
+        Button button = ( Button )findViewById( R.id.button2 ) ;
+        button.setText( "comment : " + Message.split(" ")[0] ) ;
         TextView textView = ( TextView )findViewById( R.id.textView_test ) ;
-        textView.setText(Message) ;
+        textView.setText( "balance : " + Message.split(" ")[1] ) ;
+        card = new Card( ) ;
+        card.setCreateTime( new Date() ) ;
+        card.setComments(Message.split(" ")[0]) ;
+        card.setBalance(Double.parseDouble(Message.split(" ")[1])) ;
+        cardDao = daoSession.getCardDao() ;
+        cardDao.insert( card ) ;
 
 //        DaoMaster.DevOpenHelper helper = new DaoMaster.DevOpenHelper(this, "test-db", null);
 //        db = helper.getWritableDatabase();
@@ -47,6 +66,19 @@ public class MainActivity extends AppCompatActivity {
 
     public void showButtonClicked( View view ) {
         System.out.println("[+] This is showButtonClicked function" + new Date().toString()) ;
+        cardDao = daoSession.getCardDao() ;
+        String message = "" ;
+        List<Card> cards = cardDao.loadAll() ;
+        for( int i = 0 ; i < cards.size() ; i ++ )
+        {
+//            message +=  cards.get(i).getId() + " -- id\n" ;
+            message +=  cards.get(i).getComments() + " -- 注释\n" +
+                        cards.get(i).getBalance() + " -- 余额\n" +
+                        new SimpleDateFormat("yyyy/MM/dd-HH:mm:ss").
+                            format(cards.get(i).getCreateTime()) + " -- 添加时间\n\n" ;
+        }
+        TextView textView = ( TextView )findViewById( R.id.textView_test ) ;
+        textView.setText( message ) ;
 //        DaoMaster.DevOpenHelper helper = new DaoMaster.DevOpenHelper(this, "test-db", null);
 //        db = helper.getWritableDatabase();
 //        daoMaster= new DaoMaster( db ) ;
@@ -64,6 +96,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        DaoMaster.DevOpenHelper helper = new DaoMaster.DevOpenHelper(this, "test-db", null);
+        db = helper.getWritableDatabase();
+        daoMaster= new DaoMaster( db ) ;
+        daoSession = daoMaster.newSession() ;
     }
 
     @Override
